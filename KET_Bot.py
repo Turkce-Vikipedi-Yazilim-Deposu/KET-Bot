a8d810e913b196328b23080ed40fc13546f26759
Evrifaessa <yagizhan49@protonmail.com>
2020-06-26 10:08:58 +0300

    Initial Commit.

---
+++ KET_Bot.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+import re
+import socket
+import time
+from datetime import datetime
+import platform
+
+import mavri
+
+wiki = 'tr.wikipedia'
+xx = mavri.login(wiki, 'KET Bot')
+title = 'Vikipedi:Kullanıcı engelleme talepleri'
+version = 'V3.0a'
+summary_ek = " (KET Bot, " + version + " running on " + platform.system() + "), ([[Kullanıcı mesaj:Evrifaessa|hata bildir]])"
+section = 1
+ignore_list=[]
+mpa = dict.fromkeys(range(32))
+
+while 1:
+    try:
+        datetime_now = datetime.now()
+        content = mavri.content_of_section(wiki, title, section, xx)
+
+        if content != '':
+            vandal = re.findall('\{\{\s*[Vv]andal\s*\|\s*([^\}]*)\s*\}\}', content)
+
+            if vandal:
+                timestamp = re.findall('\{\{\s*User:Evrifaessa\/KET\s*\|\s*([^\|\}]*)\s*\|\s*[^\|\}]*\s*\}\}', content)[0]
+                informer = re.findall('\{\{\s*User:Evrifaessa\/KET\s*\|\s*[^\|\}]*\s*\|\s*([^\|\}]*)\s*\}\}', content)[0]
+                vandal = vandal[0]
+                blocked = mavri.blocked(wiki, vandal)
+                not_time = datetime(int(timestamp[:4]), int(timestamp[4:6]), int(timestamp[6:8]), int(timestamp[8:10]),
+                                    int(timestamp[10:12]), int(timestamp[12:14]))
+                vandal = vandal.translate(mpa)
+
+                try:
+                    socket.inet_aton(vandal)
+                    IP = 1
+                except socket.error:
+                    IP = 0
+
+
+                if blocked.json()['query']['blocks']:
+                    timestamp = blocked.json()['query']['blocks'][0]['timestamp']
+                    blocked_time = datetime(int(timestamp[:4]), int(timestamp[5:7]), int(timestamp[8:10]), int(timestamp[11:13]), int(timestamp[14:16]), int(timestamp[17:19]))
+                    blocked_now = datetime_now - blocked_time
+                    elapsed_time = str(blocked_time - not_time).replace('days,', 'gün')
+                    by = blocked.json()['query']['blocks'][0]['by']
+                    reason = blocked.json()['query']['blocks'][0]['reason']
+
+                    summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] engellenmiş. [[Kullanıcı:' + by + '|' + by + ']] - ' + reason + summary_ek
+                    if blocked_now.seconds / 900 >= 1:
+                        mavri.section_clear(wiki, title, section, summary, xx)
+                    else:
+                        print("Gerekli süre geçmediği için atlandı.")
+
+
+                    ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:Evrifaessa Bot/KETB Karalistesi')
+                    ignore_list= re.split('\s*\*\s*', ignore_page)
+                    if informer not in ignore_list and blocked_now.seconds / 900 >= 1:
+                        message = '\n\n== Kullanıcı Engel Talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan ' + elapsed_time + ' saat sonra [[Kullanıcı mesaj:' + by + '|' + by + ']] tarafından engellendi. Engel açıklaması :' + reason + '. Bildirimde bulunduğunuz için teşekkürler.--~~~~'
+                        summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']], [[Kullanıcı mesaj:' + by + '|' + by + ']] tarafından engellendi.' + summary_ek
+                        mavri.sent_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
+                else:
+                    now = datetime.now()
+                    diff = now - not_time
+
+                    if IP and diff.total_seconds() > 60 * 60 * 24:
+                        summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
+                        mavri.section_clear(wiki, title, section, summary, xx)
+
+                        
+                        ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:Evrifaessa Bot/KETB Karalistesi')
+                        ignore_list= re.split('\s*\*\s*', ignore_page)
+                        if informer not in ignore_list:
+                            message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 24 saat geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
+                            summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
+                            mavri.sent_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
+                    if IP == 0 and diff.total_seconds() > 60 * 60 * 24 * 5:
+                        summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
+                        mavri.section_clear(wiki, title, section, summary, xx)
+                        
+                        ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:Evrifaessa Bot/KETB Karalistesi')
+                        ignore_list= re.split('\s*\*\s*', ignore_page)
+                        if informer not in ignore_list:
+                            message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 5 gün geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
+                            summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
+                            mavri.sent_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
+            else:
+                mavri.section_clear(wiki, title, section, '{{Vandal|XXXX}} içermeyen başlık temizlendi.' + summary_ek, xx)
+            section += 1
+        else:
+            section = 1
+            time.sleep(60)
+    except IndexError:
+        pass
---
+++ mavri.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+import json
+import os
+import re
+import sys
+
+import requests
+
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
+
+def login(wiki, username):
+    full_path = os.path.realpath(__file__)
+    with open(os.path.dirname(full_path) + '/.pass') as data_file:
+        data = json.load(data_file)
+    passw = data[username].decode('base64').decode('base64').decode('base64').decode('UTF-8')
+
+    payload = {'action': 'query', 'format': 'json', 'utf8': '', 'meta': 'tokens', 'type': 'login'}
+    r1 = requests.post('https://' + wiki + '.org/w/api.php', data=payload)
+
+    login_token = r1.json()['query']['tokens']['logintoken']
+    payload = {'action': 'login', 'format': 'json', 'utf8': '', 'lgname': username, 'lgpassword': passw,
+               'lgtoken': login_token}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=r1.cookies)
+
+
+def appendtext_on_page(wiki, title, appendtext, summary, xx):
+    params3 = '?format=json&action=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    edit_token = r3.json()['tokens']['edittoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'edit', 'assert': 'user', 'format': 'json', 'utf8': '', 'appendtext': appendtext,
+               'summary': summary,
+               'title': title, 'token': edit_token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def wikibase_item(wiki, title):
+    try:
+        return requests.get(
+            'https://' + wiki + '.org/w/api.php?action=query&utf8&format=json&prop=pageprops&titles=' + title).json()[
+            'query']['pages'].itervalues().next()['pageprops']['wikibase_item']
+    except:
+        return ''
+
+
+def sent_message(wiki, title, appendtext, summary, xx):
+    return appendtext_on_page(wiki, title, appendtext, summary, xx)
+
+
+def change_page(wiki, title, text, summary, xx):
+    params3 = '?format=json&action=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    edit_token = r3.json()['tokens']['edittoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'edit', 'assert': 'user', 'format': 'json', 'utf8': '', 'text': text,
+               'summary': summary,
+               'title': title, 'token': edit_token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def page_clear(wiki, title, summary, xx):
+    return change_page(wiki, title, '', summary, xx)
+
+
+def change_section(wiki, title, section, content, summary, xx):
+    params3 = '?format=json&action=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    edit_token = r3.json()['tokens']['edittoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'edit', 'assert': 'user', 'format': 'json', 'utf8': '', 'section': str(section),
+               'text': content,
+               'summary': summary, 'title': title, 'token': edit_token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def section_clear(wiki, title, section, summary, xx):
+    change_section(wiki, title, section, '', summary, xx)
+
+
+def blocked(wiki, vandal):
+    params4 = '?format=json&utf8=&action=query&list=blocks&bkprop=id%7Cuser%7Cby%7Ctimestamp%7Cexpiry%7Creason%7Crange%7Cflags&bkusers='
+    return requests.get('https://' + wiki + '.org/w/api.php' + params4 + vandal)
+
+
+def random_page(wiki):
+    return \
+        requests.get(
+            'https://' + wiki + '.org/w/api.php?format=json&utf8=&action=query&list=random&rnnamespace=0').json()[
+            'query']['random'][0]['title']
+
+
+def categories_on_page(wiki, title):
+    catNS = requests.get(
+        'https://' + wiki + '.org/w/api.php?format=json&utf8=&action=query&meta=siteinfo&siprop=namespaces').json()[
+        'query']['namespaces']['14']['*']
+    content = content_of_page(wiki, title)
+    return re.findall(r'\[\[\s?' + catNS + '\s?:\s?([^\[\|\]]*)\s?', content)
+
+
+def content_of_page(wiki, title):
+    return requests.get('https://' + wiki + '.org/w/index.php?title=' + title + '&action=raw').text
+
+
+def content_of_section(wiki, title, section, xx):
+    try:
+        return requests.get(
+            'https://' + wiki + '.org/w/api.php?format=json&utf8=&action=query&prop=revisions&rvprop=content&rvsection=' + str(
+                section) + '&titles=' + title).json()['query']['pages'].itervalues().next()['revisions'][0]['*']
+    except:
+        return ''
+
+
+def review_diff(wiki, diff, xx):
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+
+    review_cookies = xx.cookies.copy()
+    review_cookies.update(r3.cookies)
+    payload = {'action': 'review', 'format': 'json', 'revid': int(diff), 'token': token}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=review_cookies)
+
+
+def wbcreateclaim(entity, property, snaktype, value, xx):
+    wiki = 'www.wikidata'
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'wbcreateclaim', 'format': 'json', 'utf8': '', 'entity': entity, 'property': property,
+               'snaktype': snaktype, 'value': '"' + value + '"', 'token': token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def wbgetclaims(entity, property):
+    wiki = 'www.wikidata'
+
+    payload = {'action': 'wbgetclaims', 'format': 'json', 'utf8': '', 'entity': entity, 'property': property}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload)
+
+
+def wbsetsitelink(entity, linksite, linktitle, xx):
+    wiki = 'www.wikidata'
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'wbsetsitelink', 'format': 'json', 'utf8': '', 'id': entity, 'linksite': linksite,
+               'linktitle': linktitle, 'token': token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def wbmergeitems(fromid, toid, xx):
+    wiki = 'www.wikidata'
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'wbmergeitems', 'format': 'json', 'utf8': '', 'fromid': fromid, 'toid': toid, 'token': token,
+               'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def wbgetlanglink(entity, lang):
+    wiki = 'www.wikidata'
+
+    payload = {'action': 'wbgetentities', 'format': 'json', 'utf8': '', 'ids': entity, 'props': 'sitelinks'}
+    try:
+        return \
+            requests.post('https://' + wiki + '.org/w/api.php', data=payload).json()['entities'][
+                entity][
+                'sitelinks'][lang]['title']
+    except KeyError:
+        return ''
+
+
+def wbgetlangsofentity(entity):
+    wiki = 'www.wikidata'
+
+    payload = {'action': 'wbgetentities', 'format': 'json', 'utf8': '', 'ids': entity, 'props': 'sitelinks'}
+    try:
+        content = requests.post('https://' + wiki + '.org/w/api.php', data=payload).text
+        return re.findall('\"site\":\"([^\"]*)\"', content)
+    except KeyError:
+        return ''
+
+
+def wbremoveclaims(claim, xx):
+    wiki = 'www.wikidata'
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'wbremoveclaims', 'format': 'json', 'utf8': '', 'claim': claim, 'token': token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def wbsetclaimvalue(claim, snaktype, value, xx):
+    wiki = 'www.wikidata'
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'wbsetclaimvalue', 'format': 'json', 'utf8': '', 'claim': claim, 'snaktype': snaktype,
+               'value': '"' + value + '"', 'token': token, 'bot': ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+
+def pages_on_category(wiki, category):
+    params = '?format=json&utf8=&action=query&list=categorymembers&cmtitle=' + category + '&cmlimit=5000'
+    return requests.get('https://' + wiki + '.org/w/api.php' + params).json()['query']['categorymembers']
+
+
+def embeddedin(wiki, page):
+    params = '?format=json&utf8=&action=query&list=embeddedin&eititle=' + page + '&eilimit=5000'
+    return requests.get('https://' + wiki + '.org/w/api.php' + params).json()['query']['embeddedin']
+
+def move_page(wiki, From, To, reason, xx ):
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'move', 'from': From, 'to': To, 'utf8': '', 'reason': reason, 'token': token, 'bot': '','noredirect' : ''}
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
+
+def emailuser(wiki, target, subject, text, xx ):
+    params3 = '?format=json&action=query&meta=tokens'
+    r3 = requests.get('https://' + wiki + '.org/w/api.php' + params3, cookies=xx.cookies)
+    token = r3.json()['query']['tokens']['csrftoken']
+    edit_cookie = xx.cookies.copy()
+    edit_cookie.update(r3.cookies)
+
+    payload = {'action': 'emailuser', 'format': 'json', 'target': target, 'subject': subject, 'text': text, 'utf8': '',  'token': token }
+    return requests.post('https://' + wiki + '.org/w/api.php', data=payload, cookies=edit_cookie)
