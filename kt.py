d69f2f21ebd7266c2bcb1b27bd99a47aa396662f
Evrifaessa <yagizhan49@protonmail.com>
2020-07-01 10:16:26 +0300

    Fix a bug where bot archived the subpage to wrong archive page

--- kt.py
+++ kt.py
@@ -43,15 +43,19 @@ while 1:
         for page in pages:
             pageContent = mavri.content_of_page(wiki, "Vikipedi:Kategori tartışmaları/" + page)
             timestampMonth = monthList[now.month-1]
+            preTimestampMonth = monthList[now.month-2]
             timestampYear = now.year
             archivePage = "Vikipedi:Kategori_tartışmaları/Kayıt/" + str(timestampYear) + "_" + str(timestampMonth)
+            preArchivePage = "Vikipedi:Kategori_tartışmaları/Kayıt/" + str(timestampYear) + "_" + str(preTimestampMonth)
             contentLow = pageContent.lower()
             resolved = '{{kt-son}}' in contentLow
 
             archiveContent = mavri.content_of_page(wiki, archivePage.decode('UTF-8'))
+            preArchiveContent = mavri.content_of_page(wiki, preArchivePage.decode('UTF-8'))
+            hasBeenPreArchived = '{{Vikipedi:Kategori tartışmaları/' + page + '}}' in preArchiveContent
             hasBeenArchived = '{{Vikipedi:Kategori tartışmaları/' + page + '}}' in archiveContent
 
-            if hasBeenArchived == False:
+            if hasBeenArchived == False and hasBeenPreArchived == False:
                 append = '\n' + '{{Vikipedi:Kategori tartışmaları/' + page + '}}'
                 archiveSummary = 'Arşiv sayfalarında bulunmayan KT alt sayfası arşivlere ekleniyor - ' + summary_ek
                 mavri.appendtext_on_page(wiki, archivePage.decode('UTF-8'), append, archiveSummary, xx)
