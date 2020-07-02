1c9422b4ff685adb7db472973fbfc5cda3adf23b
Evrifaessa <yagizhan49@protonmail.com>
2020-07-02 21:39:36 +0300

    Update and ORES

--- sas.py
+++ sas.py
@@ -10,7 +10,7 @@ import platform
 import mavri
 
 wiki = 'tr.wikipedia'
-username = 'Evrifaessa Bot'
+username = 'KET Bot'
 xx = mavri.login(wiki, username)
 title = 'Vikipedi:Silinmeye aday sayfalar'
 version = 'V3.0g'
@@ -42,57 +42,60 @@ while 1:
     if content != '{{/Başlık}}' and content != '{{/Başlık}}\n== Tartışmalar ==':
         pages = re.findall(regex.decode('UTF-8'), content)
         for page in pages:
-            pageContent = mavri.content_of_page(wiki, "Vikipedi:Silinmeye aday sayfalar/" + page)
-            timestampMonth = monthList[now.month-1]
-            preTimestampMonth = monthList[now.month-2]
-            timestampYear = now.year
-            archivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(timestampMonth)
-            preArchivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(preTimestampMonth)
-            contentLow = pageContent.lower()
-            resolved = '{{sas son}}' in contentLow
+            try:
+                pageContent = mavri.content_of_page(wiki, "Vikipedi:Silinmeye_aday_sayfalar/" + page.decode('UTF-8'))
+                timestampMonth = monthList[now.month-1]
+                preTimestampMonth = monthList[now.month-2]
+                timestampYear = now.year
+                archivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(timestampMonth)
+                preArchivePage = "Vikipedi:Silinmeye_aday_sayfalar/Kayıt/" + str(timestampYear) + "_" + str(preTimestampMonth)
+                contentLow = pageContent.lower()
+                resolved = '{{sas son}}' in contentLow
 
-            content2 = pageContent
+                content2 = pageContent
 
-            content2 = content2.replace("Ocak", "January")
-            content2 = content2.replace("Şubat", "February")
-            content2 = content2.replace("Mart", "March")
-            content2 = content2.replace("Nisan", "April")
-            content2 = content2.replace("Mayıs", "May")
-            content2 = content2.replace("Haziran", "June")
-            content2 = content2.replace("Temmuz", "July")
-            content2 = content2.replace("Ağustos", "August")
-            content2 = content2.replace("Eylül", "September")
-            content2 = content2.replace("Ekim", "October")
-            content2 = content2.replace("Kasım", "November")
-            content2 = content2.replace("Aralık", "December")
+                content2 = content2.replace("Ocak", "January")
+                content2 = content2.replace("Şubat", "February")
+                content2 = content2.replace("Mart", "March")
+                content2 = content2.replace("Nisan", "April")
+                content2 = content2.replace("Mayıs", "May")
+                content2 = content2.replace("Haziran", "June")
+                content2 = content2.replace("Temmuz", "July")
+                content2 = content2.replace("Ağustos", "August")
+                content2 = content2.replace("Eylül", "September")
+                content2 = content2.replace("Ekim", "October")
+                content2 = content2.replace("Kasım", "November")
+                content2 = content2.replace("Aralık", "December")
 
-            regex = r"\d{2}\.\d{2}\,\s\d{1,2}\s\w+\s\d{4}\s\(UTC\)"
-            matches = re.finditer(regex, content2.decode('UTF-8'), re.MULTILINE)
+                regex = r"\d{2}\.\d{2}\,\s\d{1,2}\s\w+\s\d{4}\s\(UTC\)"
+                matches = re.finditer(regex, content2.decode('UTF-8'), re.MULTILINE)
 
-            signatureTimes = []
+                signatureTimes = []
 
-            for matchNum, match in enumerate(matches, start=1):
-                date_time_obj = datetime.strptime(str(match.group()), '%H.%M, %d %B %Y (%Z)')
-                signatureTimes.append(date_time_obj)
-                
-            youngest = min(dt for dt in signatureTimes if dt < now)
-            youngestDiff = now - youngest
+                for matchNum, match in enumerate(matches, start=1):
+                    date_time_obj = datetime.strptime(str(match.group()), '%H.%M, %d %B %Y (%Z)')
+                    signatureTimes.append(date_time_obj)
+                    
+                youngest = min(dt for dt in signatureTimes if dt < now)
+                youngestDiff = now - youngest
 
-            archiveContent = mavri.content_of_page(wiki, archivePage.decode('UTF-8'))
-            preArchiveContent = mavri.content_of_page(wiki, preArchivePage.decode('UTF-8'))
-            hasBeenPreArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in preArchiveContent
-            hasBeenArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in archiveContent
+                archiveContent = mavri.content_of_page(wiki, archivePage.decode('UTF-8'))
+                preArchiveContent = mavri.content_of_page(wiki, preArchivePage.decode('UTF-8'))
+                hasBeenPreArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in preArchiveContent
+                hasBeenArchived = '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}' in archiveContent
 
-            if hasBeenArchived == False and hasBeenPreArchived == False and youngestDiff.total_seconds() >= 60 * 60 * 12:
-                append = '\n' + '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}'
-                archiveSummary = 'Arşiv sayfalarında bulunmayan SAS alt sayfası arşivlere ekleniyor - ' + summary_ek
-                mavri.appendtext_on_page(wiki, archivePage.decode('UTF-8'), append, archiveSummary, xx)
-                print(page + ' arşiv sayfasına eklendi.')
+                if hasBeenArchived == False and hasBeenPreArchived == False:
+                    append = '\n' + '{{Vikipedi:Silinmeye aday sayfalar/' + page + '}}'
+                    archiveSummary = 'Arşiv sayfalarında bulunmayan SAS alt sayfası arşivlere ekleniyor - ' + summary_ek
+                    mavri.appendtext_on_page(wiki, archivePage.decode('UTF-8'), append, archiveSummary, xx)
+                    print(page + ' arşiv sayfasına eklendi.')
 
-            if resolved:
-                summary = 'Sonuçlandırılan SAS arşivleniyor - ' + summary_ek
-                print(page + " SAS sayfasından kaldırılıyor.")
-                newContent = content.replace("{{Vikipedi:Silinmeye aday sayfalar/" + page + "}}", "")
-                mavri.change_page(wiki, title, newContent, summary, xx)
+                if resolved and youngestDiff.total_seconds() >= 60 * 60 * 12:
+                    summary = 'Sonuçlandırılan SAS arşivleniyor - ' + summary_ek
+                    print(page + " SAS sayfasından kaldırılıyor.")
+                    newContent = content.replace("{{Vikipedi:Silinmeye aday sayfalar/" + page + "}}", "")
+                    mavri.change_page(wiki, title, newContent, summary, xx)
+            except:
+                pass
     else:
         time.sleep(60)
