499c0e1f48d378c20ebf460edb3f55e06d36c860
Evrifaessa <yagizhan49@protonmail.com>
2020-07-10 20:07:23 +0300

    Signature based timestamp fetching

--- ket.py
+++ ket.py
@@ -21,6 +21,7 @@ mpa = dict.fromkeys(range(32))
 while 1:
     try:
         datetime_now = datetime.now()
+        now = datetime.now()
         content = mavri.content_of_section(wiki, title, section, xx)
 
         if content != '':
@@ -32,6 +33,8 @@ while 1:
                 if timestamp and informer:
                     timestamp = timestamp[0]
                     informer = informer[0]
+                    contentLow = content.lower()
+                    resolved = "{{yapıldı}}" in contentLow or "{{done}}" in contentLow or "{{yapılmadı}}" in contentLow or "{{yapılmadı2}}" in contentLow or "{{yapılmadı ve yapılmayacak}}" in contentLow 
                     vandal = vandal[0]
                     blocked = mavri.blocked(wiki, vandal)
                     not_time = datetime(int(timestamp[:4]), int(timestamp[4:6]), int(timestamp[6:8]), int(timestamp[8:10]),
@@ -44,6 +47,32 @@ while 1:
                     except socket.error:
                         IP = 0
 
+                    content2 = content
+
+                    content2 = content2.replace("Ocak", "January")
+                    content2 = content2.replace("Şubat", "February")
+                    content2 = content2.replace("Mart", "March")
+                    content2 = content2.replace("Nisan", "April")
+                    content2 = content2.replace("Mayıs", "May")
+                    content2 = content2.replace("Haziran", "June")
+                    content2 = content2.replace("Temmuz", "July")
+                    content2 = content2.replace("Ağustos", "August")
+                    content2 = content2.replace("Eylül", "September")
+                    content2 = content2.replace("Ekim", "October")
+                    content2 = content2.replace("Kasım", "November")
+                    content2 = content2.replace("Aralık", "December")
+
+                    regex = r"\d{2}\.\d{2}\,\s\d{1,2}\s\w+\s\d{4}\s\(UTC\)"
+                    matches = re.finditer(regex, content2.decode('UTF-8'), re.MULTILINE)
+
+                    signatureTimes = []
+
+                    for matchNum, match in enumerate(matches, start=1):
+                        date_time_obj = datetime.strptime(str(match.group()), '%H.%M, %d %B %Y (%Z)')
+                        signatureTimes.append(date_time_obj)
+                
+                    youngest = max(dt for dt in signatureTimes if dt < now)
+                    youngestDiff = now - youngest
 
                     if blocked.json()['query']['blocks']:
                         timestamp = blocked.json()['query']['blocks'][0]['timestamp']
@@ -70,27 +99,31 @@ while 1:
                         now = datetime.now()
                         diff = now - not_time
 
-                        if IP and diff.total_seconds() > 60 * 60 * 24:
-                            summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
+                        if resolved and youngestDiff.total_seconds() / 900 >= 1:
+                            summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim olumsuz sonuçlandırılmış ya da engel detayları bot tarafından alınamadı.' + summary_ek
                             mavri.section_clear(wiki, title, section, summary, xx)
+                        else:
+                            if IP and diff.total_seconds() > 60 * 60 * 24:
+                                summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
+                                mavri.section_clear(wiki, title, section, summary, xx)
 
-                            
-                            ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:KET Bot/Yoksay')
-                            ignore_list= re.split('\s*\*\s*', ignore_page)
-                            if informer not in ignore_list and informer not in localignorelist:
-                                message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 24 saat geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
-                                summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
-                                mavri.send_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
-                        if IP == 0 and diff.total_seconds() > 60 * 60 * 24 * 5:
-                            summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
-                            mavri.section_clear(wiki, title, section, summary, xx)
-                            
-                            ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:KET Bot/Yoksay')
-                            ignore_list= re.split('\s*\*\s*', ignore_page)
-                            if informer not in ignore_list:
-                                message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 5 gün geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
-                                summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
-                                mavri.send_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
+                                
+                                ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:KET Bot/Yoksay')
+                                ignore_list= re.split('\s*\*\s*', ignore_page)
+                                if informer not in ignore_list and informer not in localignorelist:
+                                    message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 24 saat geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
+                                    summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
+                                    mavri.send_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
+                            if IP == 0 and diff.total_seconds() > 60 * 60 * 24 * 5:
+                                summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] çıkartıldı. Bildirim zaman aşımına uğradı.' + summary_ek
+                                mavri.section_clear(wiki, title, section, summary, xx)
+                                
+                                ignore_page=mavri.content_of_page('tr.wikipedia', 'Kullanıcı:KET Bot/Yoksay')
+                                ignore_list= re.split('\s*\*\s*', ignore_page)
+                                if informer not in ignore_list:
+                                    message = '\n\n== Kullanıcı engel talebi bildirimi ==\nMerhaba. [[Özel:Katkılar/' + vandal + '|' + vandal + ']], siz bildirim yaptıktan sonra 5 gün geçmesine rağmen engellenmediği için sayfadan çıkartıldı. Bildirimde bulunduğunuz için teşekkürler. --~~~~'
+                                    summary = '[[Özel:Katkılar/' + vandal + '|' + vandal + ']] bildirimi zaman aşımına uğradı.' + summary_ek
+                                    mavri.send_message(wiki, 'Kullanıcı mesaj:' + informer, message, summary, xx)
             else:
                 mavri.section_clear(wiki, title, section, '{{Vandal|XXXX}} içermeyen başlık temizlendi.' + summary_ek, xx)
             section += 1
